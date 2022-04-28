ee7ceb751bba044cb81c04ce58caccde7fd01280
sajib <me.mrsajib@gmail.com>
2022-04-28 23:24:38 +0600

    port change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
