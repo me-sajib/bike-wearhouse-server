815982ab241673b588b6fd89f2b96d3fad650d68
sajib <me.mrsajib@gmail.com>
2022-04-29 12:40:32 +0600

    show first 6 items in inventory api create

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
@@ -8,5 +10,31 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("running server");
 });
+const uri = `mongodb+srv://organicInventory:${process.env.DB_PASS}@cluster0.dhxbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-app.listen(port, () => {});
+async function databaseInterface() {
+  try {
+    await client.connect();
+    const bikeCollection = client
+      .db(process.env.DB_NAME)
+      .collection(process.env.DB_COLLECTION);
+
+    // show six bike in inventory section
+    app.get("/inventory", async (req, res) => {
+      const cursor = bikeCollection.find({}).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // client.close();
+  }
+}
+databaseInterface();
+app.listen(port, () => {
+  console.log("server is running on port " + port);
+});
