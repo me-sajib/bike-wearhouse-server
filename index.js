de47742c63bb00c1ee3aa8462821a6ea0acd7923
sajib <me.mrsajib@gmail.com>
2022-05-07 13:14:20 +0600

    add to cors origin

--- index.js
+++ index.js
@@ -5,7 +5,14 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
-app.use(cors());
+
+// allow cors origin
+const corsConfig = {
+  origin: true,
+  credentials: true,
+};
+app.use(cors(corsConfig));
+app.use("*", cors(corsConfig));
 app.use(express.json());
 
 app.get("/", (req, res) => {
